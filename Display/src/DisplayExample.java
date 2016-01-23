7628fdacd9662908170587189bdbd3092397dcc0
SrMouraSilva <mateus.moura@hotmail.com>
2016-01-22 23:31:50 -0300

    Prepared display test example

--- Display/src/DisplayExample.java
+++ Display/src/DisplayExample.java
@@ -1,5 +1,3 @@
-package com.pi4j.component.display.test;
-
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -22,42 +20,83 @@ import com.pi4j.io.gpio.GpioPinDigitalOutput;
 import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 
-public class Main {
+/*
+ * #%L
+ * **********************************************************************
+ * ORGANIZATION  :  Pi4J
+ * PROJECT       :  Pi4J :: Java Examples
+ * FILENAME      :  DisplayExample.java  
+ * 
+ * This file is part of the Pi4J project. More information about 
+ * this project can be found here:  http://www.pi4j.com/
+ * **********************************************************************
+ * %%
+ * Copyright (C) 2012 - 2016 Pi4J
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
+public class DisplayExample {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Init");
+        
+        // Get a display compoment
+        // Implemented: 
+        //  - PCD8544: Nokia 3110 and 5110
+        //  - AWT: For tests in pc develop
         Display display = getDisplayComponent();
 
+        
+        //System.out.println("Test: Display single pixel.\n");
+        //display.setPixel(10, 10, Color.BLACK);
 
+        // For any display changes, if you need to show the changes
+        // call display.redraw();
+        //display.redraw();
+
+
+        // WARNING - DisplayGraphics ignore ALL manual changes (by display.setPixel())
         DisplayGraphics graphics = new DisplayGraphics(display, Color.BLACK, ColorType.BINARY);
 
+        // Cleaning the display
         System.out.println("Clear");
         graphics.clear();
 
         Thread.sleep(5000);
-        
 
-        //System.out.println("Test: Display single pixel.\n");
-        //display.setPixel(10, 10, Color.BLACK);
-        //display.redraw();
+        // Writting text in a (x, y) position
+        graphics.drawString("Pi4j!", 0, 20);
 
-        //Thread.sleep(5000);
-        //graphics.clear();
-
-        
-        System.out.println("Test: Writing text\n");
-        graphics.drawString("Macarronada", 0, 20);
-        graphics.dispose();
-        graphics.clear();
-
+        // It's possible set the font and style usign Font:
         //Font font = new Font("Serif", Font.PLAIN, 15);
         //graphics.setFont(font);
+
+        // graphics.dispose is the REDRAW method
+        // this implementation is imcompatible with Java specification :(
+        // FIXME - Create a new method redraw for redraw :P
+        graphics.dispose();
+        graphics.clear();
         
         Thread.sleep(5000);
 
-
+        // Drawing images
         System.out.println("Test: Draw image.\n");
         String baseName = System.getProperty("user.dir") + File.separator + "lib" + File.separator;
         String imageName = baseName + "pi4j-header-small3.png";
+        //String imageName = baseName + "test.png";
 
         try {
             Image image = ImageIO.read(new File(imageName));
@@ -66,11 +105,13 @@ public class Main {
             graphics.dispose();
             graphics.clear();
         } catch (IOException e) {
+            System.err.println("Possibly image was not found :/");
             e.printStackTrace();
         }
 
         Thread.sleep(5000);
         
+        // Line, rectangle, circle (oval) tests
 
         System.out.println("Test: Draw many lines.\n");
         for (int i=0; i<84; i+=4) {
@@ -119,27 +160,15 @@ public class Main {
         graphics.clear();
         Thread.sleep(5000);
         
-        
-        /*
-        System.out.println("Test: Draw the first ~120 chars.\n");
-        for (int i=0; i < 64; i++) {
-            drawer.LCDDrawChar((i % 14) * 6, (i/14) * 8, (char)i);
-        }        
-        display.refresh();
-        Thread.sleep(5000);
-        for (int i=0; i < 64; i++)
-            drawer.LCDDrawChar((i % 14) * 6, (i/14) * 8, (char)(i + 64));
-
-        display.refresh();
-        Thread.sleep(5000);
-        graphics.clear();
-        */
+        // The Java Graphics2D api has more methods
+        // try it!
     }
 
     private static Display getDisplayComponent() {
         return new AWTDisplayComponent(500, 400);
         
-        /** /
+        /* /
+         PCD8544 example
         GpioController gpio = GpioFactory.getInstance();
 
         GpioPinDigitalOutput RST = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_15, PinState.LOW);
@@ -154,7 +183,7 @@ public class Main {
             DC,
             RST,
             SCE,
-            (byte) 60/*0xB0* /,
+            (byte) 60,
             false
         );
         /**/
